ea2726595e8336767b8f26417c3bc7dcee08bbcf
Michael Moreira <michaelmoreiramelo@gmail.com>
2024-10-18 09:25:15 -0300

    Update and rename 8-modify_zypper_conf.sh to 8-modify_zypp_conf.sh

--- 8-modify_zypp_conf.sh
+++ 8-modify_zypp_conf.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Definir o caminho do arquivo
-FILE="/etc/zypper/zypper.conf"
+# Definir o caminho correto do arquivo
+FILE="/etc/zypp/zypp.conf"
 
 # Fazer backup do arquivo antes de qualquer modificação
-BACKUP_FILE="/etc/zypper/zypper.conf.bak.$(date +%F_%T)"
+BACKUP_FILE="/etc/zypp/zypp.conf.bak.$(date +%F_%T)"
 cp $FILE $BACKUP_FILE
 echo "Backup do arquivo criado em $BACKUP_FILE"
 
 # Remover 'latest -1' da linha 'multiversion.kernels' e ajustar para 'latest,running'
-sed -i 's/multiversion.kernels = latest -1,multiversion.kernels = latest,running/' $FILE
+sed -i 's/multiversion.kernels = latest -1/multiversion.kernels = latest,running/' $FILE
 
 # Verificar se a modificação foi aplicada
 if grep -q "multiversion.kernels = latest,running" "$FILE"; then
